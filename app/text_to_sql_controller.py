5c30480be4e901501eb88ba72fa12d75280d9760
Lucas Mallmann <lucasmallmann76@gmail.com>
2023-05-16 19:43:28 -0300

    use llamaindex to retrieve database information

--- app/text_to_sql_controller.py
+++ app/text_to_sql_controller.py
@@ -1,27 +1,38 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, make_response, jsonify
 from llama_index import GPTSQLStructStoreIndex, SQLDatabase
-import json
 
 from app.db import Database
 import app.config as config
 
-text_to_sql_bp = Blueprint(
-    'text_to_sql', __name__, url_prefix='/llamaindex', template_folder=config.TEMPLATE_DIR)
+text_to_sql_bp = Blueprint('text_to_sql', __name__, url_prefix='/llamaindex')
 
 db = Database(config.SQL_DATABASE_URI)
 
-table_names = db.get_tables()
-database_columns_config = [
-    {table_name: db.get_columns(table_name)} for table_name in table_names]
 
-sql_database = SQLDatabase(db.engine, include_tables=table_names)
-index = GPTSQLStructStoreIndex(sql_database=sql_database)
-query_engine = index.as_query_engine()
+def create_index():
+    table_names = db.get_tables()
+    sql_database = SQLDatabase(db.engine, include_tables=table_names)
+    index = GPTSQLStructStoreIndex(sql_database=sql_database)
+    return index.as_query_engine()
 
 
-@text_to_sql_bp.route('/')
+@text_to_sql_bp.route('/query', methods=['GET', ])
 def index():
-    return render_template(
-        'llamaindex.html',
-        database_tables=json.dumps(database_columns_config),
-    )
+    global manager
+    query_text = request.args.get("text", None)
+    if query_text is None:
+        return ("No text found, please include a ?text=blah parameter in the URL", 400)
+    query_index = create_index()
+    response = query_index.query(query_text)
+    result = response.extra_info['result']
+
+    if len(result) <= 0:
+        return make_response(jsonify({"text": "No results found"})), 200
+
+    transformed_result = [item._asdict() for item in result]
+
+    response_json = {
+        "sql_query": response.extra_info['sql_query'],
+        "result": transformed_result,
+    }
+    return make_response(jsonify(response_json)), 200
